043d2ca6aefe916eb16248ca99863a9763728ee7
shraddha <shraddhat631@gmail.com>
2021-06-21 21:52:44 -0700

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+let burger=document.querySelector('.burger');
+let navbar=document.querySelector('.div');
+let right=document.querySelector('.unl');
+burger.addEventListener('click', ()=>{
+right.classList.toggle('v-class');
+navbar.classList.toggle('h-nav');
+})
